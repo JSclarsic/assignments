2c5812e3baf0bb2ef5709ee60300c04a0b21d3b8
Sclarsic <sclarsic@gmail.com>
2019-01-09 11:54:34 -0500

    fixed code

--- calculator.py
+++ calculator.py
@@ -1,10 +1,12 @@
-def math(a,b):
-	math1 = a*b
-	math2 = a+b
-	math3 = a-b
-	math4 = a/b
-	return math1, math2, math3, math4
+def multiply (a,b):
+	return a*b
+def add (a,b):
+	return a+b
+def subtract (a,b):
+	return a-b
+def divide (a,b):
+	return a/b
 	
 print("I'm going use the calculator functions to multiply 5 and 6")
-x = math(5,6)
+x = multiply(5,6)
 print(x)
